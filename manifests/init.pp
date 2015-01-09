6db8c471121697ac0e9695220b0d814a7f093e63
Tom Williams <tom.williams@redbookconnect.com>
2015-01-09 03:30:46 -0500

    * PD1126 - escaped double quotes inside single quotes inside double quotes, escaped....

--- manifests/init.pp
+++ manifests/init.pp
@@ -251,7 +251,7 @@ class opendj (
     validate_string($scope)
     validate_string($aci)
     $reqs               = [ Service['opendj'], File[$schema_deps], ]
-    $cmd                = "/bin/su ${user} -c '${dsconfig} set-access-control-handler-prop --${operation} ${scope}:${aci}'"
+    $cmd                = "/bin/su ${user} -c \"${dsconfig} set-access-control-handler-prop --${operation} '${scope}:${aci}'\""
     $test               = "${ldapsearch} -b '${bdn}' '(ds-cfg-${scope}=*${description}*)' ds-cfg-${scope} | sed ':a;/^[^ ]/{N;s/\n //;ba}' | fgrep -q '${aci}'"
     $nam                = "${operation}_${scope}_aci_${description}"
     $bdn                = 'cn=Access Control Handler,cn=config'
